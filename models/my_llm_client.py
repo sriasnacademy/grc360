c3e2cdd65aac8477977e2805699e1cb10ae76410
Sirisha <146825096+projectsiri@users.noreply.github.com>
2025-12-30 09:23:10 +0530

    Process and Risk Integration

--- models/my_llm_client.py
+++ models/my_llm_client.py
@@ -1,32 +1,30 @@
-from huggingface_hub import InferenceClient
-
+from groq import Groq
 
 class LLMClient:
-    def __init__(self):
-        self.client = InferenceClient(
-            model="meta-llama/Meta-Llama-3-8B-Instruct",
-            token="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"
-        )
 
-    def generate(self, prompt: str) -> str:
-        print("⚡ Calling Hugging Face LLM...")
+    def __init__(self):
+        self.client = Groq(api_key="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")
+
+    def generate(self, prompt):
+
+        print("⚡ Calling Groq LLM...")
         print("📨 Prompt Sent:", prompt[:200], "...")
 
         try:
-            response = self.client.chat_completion(
-                messages=[
-                    {"role": "user", "content": prompt}
-                ],
-                temperature=0,
-                max_tokens=1024
+            response = self.client.chat.completions.create(
+                model="llama-3.3-70b-versatile",
+                messages=[{"role": "user", "content": prompt}],
+                temperature=0
             )
+
+            print("✅ Groq Response Received")
+            print("📦 Full Response Object:", response)
 
             content = response.choices[0].message.content.strip()
 
-            print("✅ Hugging Face Response Received")
             print("📄 LLM Output:", repr(content))
             return content
 
         except Exception as e:
-            print("❌ Hugging Face Failed:", e)
+            print("❌ Groq Failed:", e)
             return ""
