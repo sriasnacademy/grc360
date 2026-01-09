8fbbd2c12c9afe94f17ff0b8bd9232e5f79dfe9f
Sirisha <146825096+projectsiri@users.noreply.github.com>
2026-01-09 17:01:43 +0530

    Rags integration for Risk and Control

--- services/rag_retrieval_service.py
+++ services/rag_retrieval_service.py
@@ -2,7 +2,7 @@
 from services.embedding_service import get_embedding
 from connectors.lambda_pgvector import PGVectorDB
 
-def rag_find_process_ids(query: str, top_k: int = 3):
+def rag_find_process_ids(query: str,entitytype:str, top_k: int = 3):
     embedding = get_embedding(query)
     embedding_str = "[" + ",".join(f"{x:.6f}" for x in embedding) + "]"
     
@@ -10,11 +10,11 @@ def rag_find_process_ids(query: str, top_k: int = 3):
         SELECT id, entity_type,
     entity_id,
     content, 1 - (embedding <=> %s::vector) AS similarity FROM rag_documents
-        WHERE entity_type = 'PROCESS'
+        WHERE entity_type = %s
         ORDER BY embedding <=> %s::vector
         LIMIT %s
         """
-    params = [embedding_str, embedding_str, top_k]
+    params = [embedding_str, entitytype, embedding_str, top_k]
     db = PGVectorDB(function_name="grc-vectordb")
     result = db.execute(query,params)
     return result
--- services/rag_service.py
+++ services/rag_service.py
@@ -2,24 +2,18 @@ import uuid
 import json
 from connectors.lambda_pgvector import PGVectorDB
 from services.embedding_service import get_embedding
+from utils.rag_builder import build_rag_payload
 
 pgvector_db = PGVectorDB(function_name="grc-vectordb")
 
 EXPECTED_DIM = 384
 
-def save_process_to_rag(data: dict, mysql_process_id: int):
+def save_process_to_rag(entity_type: str, data: dict, mysql_id):
     try:
-        rag_text = f"""
-    Process Name: {data.get('process_name')}
-    Description: {data.get('description')}
-    Department: {data.get('department')}
-    Owner: {data.get('owner')}
-    Frequency: {data.get('frequency')}
-    Triggers: {', '.join(data.get('triggers', []))}
-    Outcomes: {', '.join(data.get('outcomes', []))}
-    """.strip()
+        
+        payload = build_rag_payload(entity_type, data)
 
-        embedding = get_embedding(rag_text)
+        embedding = get_embedding(payload["rag_text"])
         embedding_str = "[" + ",".join(f"{x:.6f}" for x in embedding) + "]"
 
         query = """
@@ -36,17 +30,18 @@ def save_process_to_rag(data: dict, mysql_process_id: int):
 
         params = (
             str(uuid.uuid4()),
-            "PROCESS",
-            str(mysql_process_id),  # 🔑 LINK HERE
-            rag_text,
+            entity_type,
+            str(mysql_id),
+            payload["rag_text"],
             embedding_str,
             json.dumps({
-                "source_table": "processes",
-                "mysql_id": mysql_process_id
+                "source_table": payload["source_table"],
+                **payload["metadata"]
             })
         )
 
         pgvector_db.execute(query, params)
+
         print("Rag Inserted Successfully")
 
     except Exception as e:
