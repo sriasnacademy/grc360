b04798f9c07b84da0abb2583ab3ac34da16bedca
Sirisha <146825096+projectsiri@users.noreply.github.com>
2026-03-02 17:26:15 +0530

    Issue WF - Completed - GUI Also Added

--- services/issue_service.py
+++ services/issue_service.py
@@ -16,7 +16,14 @@ class IssueService:
         result = call_lambda({
             "action": "insert",
             "table": "issues",
-            "data": payload
+            "data": {
+                "test_task_id":task_id,
+                "control_id": control_id,
+                "test_plan_id": test_plan_id,
+                "issue_type": "CONTROL_FAILURE",
+                "status": "OPEN",
+                "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            }
         })
 
         issue_id = result.get("inserted_id")
